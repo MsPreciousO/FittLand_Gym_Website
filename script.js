08d338b07a04aef5016920b215bce0fa3e2fb56d
MsPreciousO <opalcreations68@gmail.com>
2022-01-06 06:56:47 -0500

    continuing working on frequently asked question section - accordion

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const accordionBtns = document.querySelectorAll ('.accordion');
 
-
 accordionBtns.forEach(accordion =>{
     // console.log(accordion)
     accordion.addEventListener('click', ()=> {
         // console.log('toggle clicked')
+        
         accordion.classList.toggle("is-open");
         
         let content = accordion.nextElementSibling;
@@ -15,5 +15,10 @@ accordionBtns.forEach(accordion =>{
             content.style.maxHeight = content.scrollHeight + 'px'
         }
     })
-    
+    // if (content.style.display === 'block'){
+    //     content.style.display = 'none'; 
+    // }else{
+    //     content.style.display = 'block';
+    // }
 })
+/* Use classList to add and remove to stop it from showing when not clicked */
